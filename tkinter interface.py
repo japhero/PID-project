4bb8435dd7c7c7156f32aa84a585b7d3e966a9d4
japhero <68838158+japhero@users.noreply.github.com>
2023-04-27 08:44:01 -0400

    added new multi input version to Live graph library

---
+++ tkinter interface.py
@@ -0,0 +1,52 @@
+import time
+import tkinter as tk
+import math
+
+root = tk.Tk()
+root.geometry("300x100")
+
+scaleVal = tk.DoubleVar()
+scale = tk.Scale(from_=-10, to=10, orient=tk.HORIZONTAL, length=300, resolution=1, variable=scaleVal)
+
+scale.pack()
+
+textVar = tk.Variable()
+text = tk.Label(text="innit", textvariable=textVar)
+text.pack()
+
+mainList = ["PID on", " PID off", "setPoint"]
+mainMenu = 1
+
+PidState = False
+
+
+
+def setButtonVar(*args):
+    global buttonVar, PidState, mainMenu
+    if int(scaleVal.get()) % 3 == 2 and ( mainMenu == 1 or mainMenu ==2):
+
+        mainMenu =3
+
+    else:
+        if mainMenu == 3:
+            mainMenu =2
+        else:
+            mainMenu =1
+
+    print(mainMenu)
+
+
+btnVar = tk.IntVar()
+button = tk.Button(text="enter")
+button.bind("<ButtonPress>", setButtonVar)
+
+button.pack()
+
+while True:
+    if mainMenu == 1 or mainMenu ==2:
+        textVar.set(mainList[int(scaleVal.get() % 3)])
+    else:
+        textVar.set(f"press to exit setpoint = {abs(scaleVal.get())}")
+
+    time.sleep(.01)
+    root.update()
